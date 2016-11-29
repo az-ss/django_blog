bc5b39030713c31f018d881eefc6f443502f37f7
Aleksandr <ozelinskyi@n-ix.com>
2016-11-29 17:15:32 +0200

    Added templates, home page test view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Main page")
+    context = {'hi': 'INDEX'}
+    template = loader.get_template('home/index.html')
+    return HttpResponse(template.render(context, request))
